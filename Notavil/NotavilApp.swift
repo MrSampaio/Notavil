51a9c94d5b9a9ab6a09836efe126f0de6e7d59c2
MrSampaio <jusampa2@gmail.com>
2026-06-24 19:25:58 -0300

    Initial Commit

---
+++ Notavil/NotavilApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotavilApp.swift
+//  Notavil
+//
+//  Created by Julio Sampaio on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct NotavilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
