ed52717f152520f4524be2b772633f86f007aea1
MrSampaio <jusampa2@gmail.com>
2026-06-24 21:37:17 -0300

    feat: implement login and registration screens

--- Notavil/View/Auth/LoginView.swift
+++ Notavil/View/Auth/LoginView.swift
@@ -5,4 +5,63 @@
 //  Created by Julio Sampaio on 24/06/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        Text("Login")
+            .fontWeight(.bold)
+        Image(.swift)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 120, height: 120)
+            .padding()
+        VStack{
+            TextField("Email", text: $email)
+                .autocapitalization(.none)
+                .padding(12)
+                .background(Color(.systemGray4))
+                .foregroundColor(Color(.black))
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+            
+            SecureField("Senha", text: $password)
+                .autocapitalization(.none)
+                .padding(12)
+                .background(Color(.systemGray4))
+                .foregroundColor(Color(.white))
+                .cornerRadius(10)
+                .padding(.horizontal, 24)
+            
+            Button{} label: {
+                Text("Login")
+                    .frame(width: 350, height: 50)
+                    .background(Color(.blue))
+                    .foregroundColor(Color(.white))
+                    .fontWeight(.bold)
+                    .cornerRadius(20)
+                    
+            } .padding(.vertical)
+            
+            NavigationLink{
+                RegisterView()
+            } label: {
+                Text("Faça seu cadastro aqui")
+                    .foregroundColor(Color(.blue))
+                
+            } .padding(.vertical, 50)
+
+        }
+        
+    }
+}
+
+#Preview {
+    NavigationStack{
+        LoginView()
+    }
+   
+}
