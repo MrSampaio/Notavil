7c85ba76adbd001ccebef1f34d5e83983132fb3a
MrSampaio <jusampa2@gmail.com>
2026-06-25 21:17:55 -0300

    feat: configure AuthManager

---
+++ Notavil/Services/SupabaseAuthManager.swift
@@ -0,0 +1,52 @@
+//
+//  AuthManager.swift
+//  Notavil
+//
+//  Created by Julio Sampaio on 25/06/26.
+//
+
+import Foundation
+
+@Observable @MainActor
+
+final class AuthManager{
+    private let service: SupabaseAuthServices
+    var error: Error?
+    
+    init(service: SupabaseAuthServices){
+        self.service = service
+    }
+    
+    func login(containsEmail email: String, password: String) async {
+        do{
+            try await service.login(containsEmail: email, password: password)
+        } catch{
+            self.error = error
+            print("ERROR: Login service failed: \(error)")
+        }
+    }
+    
+    func signUp(containsEmail email: String, password: String) async {
+        do{
+            try await service.signUp(containsEmail: email, password: password)
+        } catch{
+            self.error = error
+            print("ERROR: Sign Up service failed: \(error)")
+        }
+    }
+    
+    func signOut() async throws{
+        do{
+            try await service.signOut()
+        } catch{
+            self.error = error
+            print("ERROR: Sign Out service failed: \(error)")
+        }
+        
+    }
+    
+    func getAuthStatus() async throws{
+        
+    }
+    
+}
